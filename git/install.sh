f7efc336365deffbcfbe100cd9174d3c47e2197a
tsukumo <tsu.complete@gmail.com>
2015-06-21 20:32:24 -0400

    Adds config to installer
    Prompts for username and email then
    feeds them into the retrieved config

--- git/install.sh
+++ git/install.sh
@@ -2,6 +2,25 @@
 
 echo "-GIT"
 
+retr () {
+    curl -# $1 > $2
+}
+
+inst () {
+    retr "https://raw.githubusercontent.com/tsu-complete/dot/master/$1" $2
+}
+
 echo "--global ignore"
-curl -# https://www.gitignore.io/api/linux,osx > ~/.gitignore
+retr https://www.gitignore.io/api/linux,osx ~/.gitignore
+
+echo "--global config"
+inst git/_gitconfig ~/.gitconfig
+
+read -p "---prompt: user.name  : " uname
+read -p "---prompt: user.email : " email
+
+echo "" >> ~/.gitconfig
+echo "[user]" >> ~/.gitconfig
+echo "    name = $uname" >> ~/.gitconfig
+echo "    email = $email" >> ~/.gitconfig
 

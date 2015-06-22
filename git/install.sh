9382d55ae5b15be85d9d9fb5c48b28fafec9f061
tsukumo <tsu.complete@gmail.com>
2015-06-21 21:17:31 -0400

    Cleans git installer
    Adds colored status results and removes
    progress bar

--- git/install.sh
+++ git/install.sh
@@ -1,20 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "-GIT"
 
 retr () {
-    curl -# $1 > $2
+    o=$(curl --silent -w "%{http_code}" -o $2 $1)
+    if [[ $o == 2* ]];then
+        echo -n " [32m[success][0m"
+    elif [[ $o == 4* ]];then
+        echo -n " [32m[failure][0m"
+        exit $o
+    else
+        echo -n " [35m[unknown failure][0m"
+        exit $o
+    fi
 }
 
 inst () {
     retr "https://raw.githubusercontent.com/tsu-complete/dot/master/$1" $2
 }
 
-echo "--global ignore"
+echo -n "--global ignore"
 retr https://www.gitignore.io/api/linux,osx ~/.gitignore
+echo
 
-echo "--global config"
+echo -n "--global config"
 inst git/_gitconfig ~/.gitconfig
+echo
 
 read -p "---prompt: user.name  : " uname
 read -p "---prompt: user.email : " email
